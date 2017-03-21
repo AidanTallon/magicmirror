014ed6fb8ec998d8d2aea74d2288769fe391d318
AidanTallon <atallon@spartaglobal.co>
2017-03-21 21:34:46 +0000

    Start Forecast class

--- lib/forecast.rb
+++ lib/forecast.rb
@@ -1,8 +1,22 @@
 class Forecast
-  attr_reader :latitude, :longitude
-  
-  def initialize(lat, lon)
-    @latitude = lat
-    @longitude = lon
+  attr_reader :latitude, :longitude, :country, :city, :current
+  require 'forecast_io'
+
+  def initialize(location)
+    @latitude = location.latitude
+    @longitude = location.longitude
+    @country = location.country
+    @city = location.city
+    ForecastIO.api_key = ENV['FORECAST_IO_API_KEY']
+  end
+
+  def fetch
+    data = ForecastIO.forecast @latitude, @longitude
+    @current =
+    {
+      summary:      data['currently']['summary'],
+      icon:         data['currently']['rain'],
+      temperature:  data['currently']['temperature']
+    }
   end
 end
--- spec/forecast_spec.rb
+++ spec/forecast_spec.rb
@@ -1,7 +1,17 @@
 describe Forecast do
-  it 'should initialize given latitude and longitude' do
-    @forecast = Forecast.new @lat, @lon
-    expect(@forecast.latitude).to eq @lat
-    expect(@forecast.longitude).to eq @lon
+  it 'should initialize given an instance of Location' do
+    @forecast = Forecast.new Location.new latitude: 53, longitude: -2
+    expect(@forecast.latitude).to eq 53
+    expect(@forecast.longitude).to eq -2
+    expect(@forecast.country).to eq 'United Kingdom'
+    expect(@forecast.city).to eq 'Stoke-on-Trent'
+  end
+
+  it 'should fetch weather data' do
+    @forecast = Forecast.new Location.new latitude: 53, longitude: -2
+    @forecast.fetch
+    expect(@forecast.current).to include :summary
+    expect(@forecast.current).to include :icon
+    expect(@forecast.current).to include :temperature
   end
 end
