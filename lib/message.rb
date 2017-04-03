41df5282901da9d5e6f52b6a4cfc190497e74cf6
AidanTallon <atallon@spartaglobal.co>
2017-04-03 21:45:20 +0100

    modify message_spec to better test not generic messages

--- lib/message.rb
+++ lib/message.rb
@@ -14,7 +14,7 @@ class Message
     current_time = DateTime.now
     if current_time.hour >= 5 and current_time.hour < 12
       return :morning
-    elsif current_time.hour >= 12 and (current_time.hour <= 5 or (current_time.hour == 6 and current_time.minute <= 30))
+    elsif current_time.hour >= 12 and (current_time.hour <= 17 or (current_time.hour == 18 and current_time.minute <= 30))
       return :afternoon
     elsif (current_time.hour == 17 and current_time.hour > 30) or current_time.hour >= 18 or current_time.hour < 3 or (current_time.hour == 3 and current_time.minute == 0)
       return :evening
--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -6,49 +6,60 @@ describe Message do
   end
 
   it 'should return a generic/morning specific message between 5am and 11:59am' do
+    messages_returned = []
     10.times do
       hour = rand(5) + 6
       minute = rand(60)
       Timecop.freeze(DateTime.parse("12/12/12 #{hour}:#{minute}am")) do
-        expect(test_data['morning'] + test_data['generic']).to include message.get
+        messages_returned << message.get
       end
     end
+    expect(messages_returned).to include *test_data['morning']
+    expect(messages_returned).to include *test_data['generic']
   end
 
   it 'should return a generic/afternoon specific message between 12pm and 5:30pm' do
+    messages_returned = []
     10.times do
       hour = rand(6)
       minute = rand(31)
       Timecop.freeze(DateTime.parse("12/12/12 #{hour}:#{minute}pm")) do
-        expect(test_data['afternoon'] + test_data['generic']).to include message.get
+        messages_returned << message.get
       end
     end
+    expect(messages_returned).to include *test_data['afternoon']
+    expect(messages_returned).to include *test_data['generic']
   end
 
   it 'should return a generic/evening specific message between 5:31pm and 3am' do
+    messages_returned = []
     5.times do
       hour = rand(7) + 5
       minute = rand(32) 
       Timecop.freeze(DateTime.parse("12/12/12 #{hour}:#{minute}pm")) do
-        expect(test_data['evening'] + test_data['generic']).to include message.get
+        messages_returned << message.get
       end
     end
     5.times do
       hour = rand(3)
       minute = rand(60)
       Timecop.freeze(DateTime.parse("12/12/12 #{hour}:#{minute}am")) do
-        expect(test_data['evening'] + test_data['generic']).to include message.get
+        messages_returned << message.get
       end
     end
+    expect(messages_returned).to include *test_data['evening']
+    expect(messages_returned).to include *test_data['generic']
   end
 
   it 'should return a generic message between 3:01am and 4:59am' do
+    messages_returned = []
     10.times do
       hour = rand(1) + 3
       minute = rand(58) + 1
       Timecop.freeze(DateTime.parse("12/12/12 #{hour}:#{minute}am")) do
-        expect(test_data['generic']).to include message.get
+        messages_returned << message.get
       end
     end
+    expect(messages_returned).to include *test_data['generic']
   end
 end
